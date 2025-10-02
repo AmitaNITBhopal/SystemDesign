e7f3b4b6a975ca14c70f5a0b744eabae4120b627
AmitaNITBhopal <amita.nitbhopal@gmail.com>
2025-10-02 12:51:22 +0200

    Adding sample projects for System Design Tutorial

---
+++ BasicLOS/src/main/java/com/systemdesign/BasicLOS/BasicLosApplication.java
@@ -0,0 +1,13 @@
+package com.systemdesign.BasicLOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicLosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicLosApplication.class, args);
+	}
+
+}
---
+++ BasicLOS/src/test/java/com/systemdesign/BasicLOS/BasicLosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemdesign.BasicLOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicLosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
